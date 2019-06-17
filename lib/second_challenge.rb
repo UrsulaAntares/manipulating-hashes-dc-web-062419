6723371f786fdd05816f61b518a140db89504dbe
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 20:39:28 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,7 +8,7 @@ def second_challenge
 
   #code your solution here!
   all_dem_ingredients = []
-  groceries.each do |key, values|
+#  groceries.each do |key, values|
   all_dem_ingredients << groceries.values
   end
   all_dem_ingredients.flatten!
