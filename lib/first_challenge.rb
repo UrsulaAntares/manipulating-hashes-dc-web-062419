0a3488a5a8e44fabbdf5e60d7f9459d52065568c
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 20:19:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
     if person == "Freddy Mercury"
       contacts.each do |attribute, thing|
         if attribute = :favorite_icecream_flavors
-          put :favorite_icecream_flavors
+          put thing
           thing.delete("strawberry")
           attribute
         end
